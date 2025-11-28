334317add213ee0e14f26010f818c77ac90d7a6c
kamleshsaran371 <kamleshsaran371@gmail.com>
2025-11-28 23:15:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("✨ Commands", callback_data="cmd_command")],
             [InlineKeyboardButton("💎 Features", callback_data="feat_command"), InlineKeyboardButton("⚙️ Settings", callback_data="setttings")],
             [InlineKeyboardButton("💳 Plans", callback_data="upgrade_command")],
-            [InlineKeyboardButton(text="📞 Contact", url=f"tg://openmessage?user_id={OWNER}"), InlineKeyboardButton(text="🛠️ Repo", url="https://github.com/nikhilsainiop/saini-txt-direct")],
-        ])      
+            [InlineKeyboardButton(text="📞 Contact", url=f"tg://openmessage?user_id={OWNER}")],
 
 @bot.on_message(filters.command("start"))
 async def start(bot, m: Message):
