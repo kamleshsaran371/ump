648f198f2bb8129f89bfda950bfd5d5ca9e214b2
kamleshsaran371 <kamleshsaran371@gmail.com>
2025-11-29 00:46:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ bot = Client(
 
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
 keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("✨ Commands", callback_data="cmd_command")],
-            [InlineKeyboardButton("💎 Features", callback_data="feat_command"), InlineKeyboardButton("⚙️ Settings", callback_data="setttings")],
-            [InlineKeyboardButton("💳 Plans", callback_data="upgrade_command")],
-            [InlineKeyboardButton(text="📞 Contact", url=f"tg://openmessage?user_id={OWNER}")],
-
+    [InlineKeyboardButton("✨ Commands", callback_data="cmd_command")],
+    [InlineKeyboardButton("💡 Features", callback_data="feat_command"), InlineKeyboardButton("⚙️ Settings", callback_data="settings_command")],
+    [InlineKeyboardButton("🟹 Plans", callback_data="upgrade_command")],
+    [InlineKeyboardButton("📖 Contact", url=f"tg://openmessage?user_id={OAMER}")]
+])
 @bot.on_message(filters.command("start"))
 async def start(bot, m: Message):
     user_id = m.chat.id
